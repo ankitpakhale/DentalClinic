b555bc9af652fe55b49219f03793a993fb9b117e
Ankit Pakhale <akp3067@gmail.com>
2023-03-13 14:48:47 +0530

    Added: cdn

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import "./Assets/css/bootstrap.min.css"
+import "./Assets/css/style.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        {/* <Navbar /> */}
+        <Routes>
+          {/* <Route path="/" exact element={<Index />} />
+          <Route path="/about" exact element={<About />} />
+          <Route path="/contact" exact element={<Contact />} />
+          <Route path="/booking" exact element={<Booking />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/register" exact element={<Register />} />
+          <Route path="/upcomingmovies" exact element={<UpcomingMovie />} /> */}
+        </Routes>
+        {/* <Footer /> */}
+      </Router>
+    </>
   );
 }
 
